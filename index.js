15364bc736f29ff0a8567e2befdb9e9cbe6d533c
FERDIZ-afk <ferdizakyla@gmail.com>
2023-03-17 10:31:24 +0700

    update

--- index.js
+++ index.js
@@ -60,8 +60,60 @@ const utils = require("./utils");
 const { prefixbot } = require('./config/settings')
 const msgRetryCounterMap = MessageRetryMap || { }
 
+var low
+try {
+	low = require('lowdb')
+} catch (e) {
+	low = require('./lib/lowdb')
+}
+
+const {
+	Low,
+	JSONFile
+} = low
+const _ = require('lodash')
+const mongoDB = require('./lib/mongoDB')
+const cloudDBAdapter = require('./lib/cloudDBAdapter')
+//global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+global.db = new Low(
+	//	/https?:\/\//.test(opts['db'] || '') ?
+	//new cloudDBAdapter(opts['db']) : /mongodb/.test(opts['db']) ?
+	//new mongoDB(mongoURI) 
+	new JSONFile(`./database/dbbot.json`)
+)
 
 
+global.DATABASE = global.db // Backwards Compatibility
+global.loadDatabase = async function loadDatabase() {
+	if (global.db.READ) return new Promise((resolve) => setInterval(function() {
+		(!global.db.READ ? (clearInterval(this), resolve(global.db.data == null ? global.loadDatabase() : global.db.data)) : null)
+	}, 1 * 1000))
+	if (global.db.data !== null) return
+	global.db.READ = true
+	await global.db.read()
+	global.db.READ = false
+	global.db.data = {
+		users: {
+		  contacts : {},
+		},
+		group: {
+		  antilink : [],
+		  antiviewonce : [],
+		  antidelete : [],
+		},
+		donate: { 
+		  saweria : []
+		},
+		menfess: {},
+		...(global.db.data || {})
+	}
+	global.db.chain = _.chain(global.db.data)
+}
+loadDatabase()
+
+if (global.db) setInterval(async () => {
+	if (global.db.data) await global.db.write()
+}, 30 * 1000)
 
 const attribute = {};
 attribute.prefix = prefixbot 
